789563868b3c4d469d0d9209164c6d3cdccffe89
Meet Raut <meetraut3004@gmail.com>
2024-09-28 01:03:13 +0530

    almost done

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -9,6 +9,8 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from '../../hook/useFetch';
 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 
 const JobDetails = () => {
   const params = useLocalSearchParams();
@@ -19,9 +21,32 @@ const JobDetails = () => {
   })
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {
 
+  }
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return <Specifics 
+          title="Qualifications"
+          points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+        />
+      case "About":
+        return <JobAbout 
+          info={data[0].job_description ?? "No data provided"}
+        />
+      case "Responsibilities":
+        return <Specifics 
+        title="Responsibilities"
+        points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+      />
+
+      default:
+        break;
+    }
   }
 
   return (
@@ -61,12 +86,19 @@ const JobDetails = () => {
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
               <Company 
-                
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                Location={data[0].job_country}
               />
 
               <JobTabs 
-
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
 
